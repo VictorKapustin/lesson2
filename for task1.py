18e057fa7be1042c3ab7627932cef1c57a3f6c02
Victor Kapustin <vickapustin@gmail.com>
2019-02-27 19:05:52 +0300

    Average school score fixed

--- for task1.py
+++ for task1.py
@@ -4,7 +4,7 @@ classes_scores = [{'school_class': '4a', 'scores': [3, 4, 4, 5, 2]},
                   ]
 
 class_average = {}
-school_average = 0
+school_average = []
 
 for school_class in classes_scores:  # Итерация по словарям в списке
 
@@ -12,14 +12,14 @@ for school_class in classes_scores:  # Итерация по словарям в
     # Итерация по оценке в списке с оценками
     for score in school_class['scores']:
         class_sum_scores += score
+        school_average.append(score)
     class_average_score = class_sum_scores / len(school_class['scores'])
 
     # Добавление в словарь класс: средняя оценка
     class_average[school_class['school_class']] = class_average_score
 
-    school_average += class_average_score
 
 print('School average score is ' +
-      str(school_average / len(classes_scores)) + '\n')
+      str(sum(school_average) / len(school_average)) + '\n')
 for key, value in class_average.items():
     print('Average score in {} class is {}'.format(key, value))
